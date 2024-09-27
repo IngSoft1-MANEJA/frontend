51e9fa653025cf6d9c5c9d9bcf8e92c50280e952
Alfredo Viera <alfredo.viera@mi.unc.edu.ar>
2024-09-27 01:14:19 -0300

    SWT-12 agrega funcionalidad para unirse a partida

--- src/containers/App/components/JoinMatchButton.jsx
+++ src/containers/App/components/JoinMatchButton.jsx
@@ -1,70 +1,90 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
+import { MatchService } from "../../../services/MatchService";
 
-function JoinMatchButton(props) {
-    const [ userName, setUserName ] = useState("");
-    const [ errorMessage, setErrorMessage ] = useState("");
-    const navigate = useNavigate();
+function JoinMatchButton({ matchId, setShowAlert, setAlertMessage }) {
+  const [userName, setUserName] = useState("");
+  const [errorMessage, setErrorMessage] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
+  const navigate = useNavigate();
 
-    const handleJoin = (e) => {
-        e.preventDefault();
-        console.log(userName);
-        if (userName) {
-            navigate("/lobby");
-        } else {
-            setErrorMessage("Por favor, ingrese un nombre de usuario");
-        }
-    };
+  const handleJoin = async (e) => {
+    e.preventDefault();
+    console.log(userName);
+    if (userName) {
+      try {
+        setIsLoading(true);
+        await MatchService.joinMatch(matchId, userName);
+        setIsLoading(false);
+        navigate("/lobby");
+      } catch (error) {
+        console.error(error.message);
+        setIsLoading(false);
+        setAlertMessage("Error al unirse a la partida");
+        setShowAlert(true);
+      }
+    } else {
+      setErrorMessage("Por favor, ingrese un nombre de usuario");
+    }
+  };
 
-    return (
-      <>
-        <button
-          className="btn"
-          onClick={() =>
-            document.getElementById("join_match_modal").showModal()
-          }
-        >
-          unirse a partida
-        </button>
-        <dialog id="join_match_modal" className="modal">
-          <div className="modal-box">
-            <form method="dialog">
-              <button
-                onClick={() => setErrorMessage("")}
-                className="btn btn-sm btn-circle btn-ghost absolute right-2 top-2"
-              >
-                ✕
+  const clearModal = () => {
+    setErrorMessage("");
+    setUserName("");
+  };
+
+  return (
+    <>
+      <button
+        className="btn"
+        onClick={() => document.getElementById("join_match_modal").showModal()}
+      >
+        unirse a partida
+      </button>
+      <dialog id="join_match_modal" className="modal">
+        <div className="modal-box">
+          <form method="dialog">
+            <button
+              onClick={clearModal}
+              className="btn btn-sm btn-circle btn-ghost absolute right-2 top-2"
+            >
+              ✕
+            </button>
+          </form>
+          <div className="flex items-center">
+            <form className="flex flex-col items-start">
+              <label className="form-control mb-6">
+                <div className="label">
+                  <span className="label-text">Ingresa tu nombre</span>
+                </div>
+                <input
+                  className={`input input-bordered ${
+                    errorMessage ? "input-error" : ""
+                  }`}
+                  type="text"
+                  placeholder="Nombre"
+                  value={userName}
+                  onChange={(e) => setUserName(e.target.value)}
+                />
+                <div className="label">
+                  <span className="label-text-alt text-error">
+                    {errorMessage}
+                  </span>
+                </div>
+              </label>
+              <button className="btn" onClick={handleJoin}>
+                {!isLoading ? (
+                  "Unirse"
+                ) : (
+                  <span className="loading loading-spinner"></span>
+                )}
               </button>
             </form>
-            <div className="flex items-center">
-              <form className="flex flex-col items-start">
-                <label className="form-control mb-6">
-                  <div className="label">
-                    <span className="label-text">Ingresa tu nombre</span>
-                  </div>
-                  <input
-                    className={`input input-bordered ${
-                      errorMessage ? "input-error" : ""
-                    }`}
-                    type="text"
-                    placeholder="Nombre"
-                    onChange={(e) => setUserName(e.target.value)}
-                  />
-                  <div className="label">
-                    <span className="label-text-alt text-error">
-                      {errorMessage}
-                    </span>
-                  </div>
-                </label>
-                <button className="btn" onClick={handleJoin}>
-                  Unirse
-                </button>
-              </form>
-            </div>
           </div>
-        </dialog>
-      </>
-    );
+        </div>
+      </dialog>
+    </>
+  );
 }
 
 export default JoinMatchButton;

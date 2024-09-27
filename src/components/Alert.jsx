aca5f0081d9b8dde062795cce69deb5989b8ce75
Alfredo Viera <alfredo.viera@mi.unc.edu.ar>
2024-09-27 01:11:06 -0300

    SWT-12 crea componente de alertas al usuario

---
+++ src/components/Alert.jsx
@@ -0,0 +1,47 @@
+function Alert({ setShowAlert, showAlert, alertMessage }) {
+  if (showAlert) {
+    return (
+      <div role="alert" className="alert alert-error">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          className="h-6 w-6 shrink-0 stroke-current"
+          fill="none"
+          viewBox="0 0 24 24"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            strokeWidth="2"
+            d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z"
+          />
+        </svg>
+        <span>{alertMessage}</span>
+        <div className="flex-none">
+          <button
+            className="btn btn-sm btn-ghost"
+            onClick={() => setShowAlert(false)}
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-6 w-6"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth={2}
+                d="M6 18L18 6M6 6l12 12"
+              />
+            </svg>
+          </button>
+        </div>
+      </div>
+    );
+  } else {
+    return null;
+  }
+}
+
+export default Alert;

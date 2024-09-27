82d4ca44916e0bac5777f0034a93459318384d1c
Alfredo Viera <alfredo.viera@mi.unc.edu.ar>
2024-09-27 01:12:08 -0300

    SWT-12 configura las alertas para mostrar al usuario

--- src/containers/App/App.jsx
+++ src/containers/App/App.jsx
@@ -1,13 +1,25 @@
-import { useState } from 'react';
-import JoinMatchButton from './components/JoinMatchButton';
+import { useState } from "react";
+import JoinMatchButton from "./components/JoinMatchButton";
+import Alert from "../../components/Alert.jsx";
 
 function App() {
+  const [alertMessage, setAlertMessage] = useState("");
+  const [showAlert, setShowAlert] = useState(false);
 
   return (
     <>
-      <JoinMatchButton></JoinMatchButton>
+      <JoinMatchButton
+        matchId={1}
+        setShowAlert={setShowAlert}
+        setAlertMessage={setAlertMessage}
+      ></JoinMatchButton>
+      <Alert
+        setShowAlert={setShowAlert}
+        showAlert={showAlert}
+        alertMessage={alertMessage}
+      ></Alert>
     </>
-  )
+  );
 }
 
-export default App
+export default App;

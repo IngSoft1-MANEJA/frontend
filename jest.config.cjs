2a10f7ffb38063db3a240a45264d78ff562e65b2
Alfredo Viera <alfredo.viera@mi.unc.edu.ar>
2024-09-28 15:00:14 -0300

    SWT-12 agrega configuracion para jest

---
+++ jest.config.cjs
@@ -0,0 +1,16 @@
+module.exports = {
+    // testEnvironment: 'jest-environment-jsdom',
+    // setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
+    testEnvironmentOptions: {
+        customExportConditions: [''],
+      },
+    transform: {
+      '^.+\\.jsx?$': 'babel-jest'
+    },
+    moduleNameMapper: {
+      '\\.(css|less)$': '<rootDir>/src/__mocks__/ModalCrearPartidaStyleMock.jsx',
+      '\\.(css|less)$': '<rootDir>/src/__mocks__/SuccessAlertStyleMock.jsx'
+    },
+    setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
+    testEnvironment: 'jest-fixed-jsdom'
+}

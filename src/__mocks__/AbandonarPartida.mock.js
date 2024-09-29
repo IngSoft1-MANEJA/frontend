985e0e1f16b309d4d882fbaed06cd08d1bf743a6
Alexis714 <136205174+Alexis714@users.noreply.github.com>
2024-09-29 16:35:32 -0300

    SWT-14 agrego Test Mocks

---
+++ src/__mocks__/AbandonarPartida.mock.js
@@ -0,0 +1,25 @@
+export const Started = {
+    estadoPartida: "STARTED",
+    idJugador: "1",
+    idPartida: "2",
+}
+
+export const WaitingNoAnfitrion = {
+    estadoPartida: "WAITING",
+    esAnfitrion: false,
+    idJugador: "1",
+    idPartida: "2",
+}
+
+export const WaitingAnfitrion = {
+    estadoPartida: "WAITING",
+    esAnfitrion: true,
+    idJugador: "1",
+    idPartida: "2",
+}
+
+export const Error = {
+    estadoPartida: "FINISHED",
+    idJugador: "1",
+    idPartida: "2",
+}
---
+++ src/__test__/AbandonarPartida.test.js
@@ -0,0 +1,65 @@
+import React from 'react';
+import { render, screen, fireEvent, cleanup, waitFor } from '@testing-library/react';
+import { AbandonarPartida } from '../containers/App/components/AbandonarPartida';
+import { Started,
+         WaitingAnfitrion,
+         WaitingNoAnfitrion,
+         Error
+} from '../__mocks__/AbandonarPartida.mock';  
+
+const mockedNavigate = jest.fn();
+
+jest.mock('react-router-dom', () => ({
+    ...jest.requireActual('react-router-dom'),
+    useNavigate: () => mockedNavigate
+}));
+
+global.fetch = jest.fn(() =>
+    Promise.resolve({
+      ok: true,
+      json: () => Promise.resolve({}),
+    }),
+  );
+
+describe("AbandonarPartida", () => {
+
+    afterEach(cleanup);
+
+    afterEach(() => {
+      jest.clearAllMocks();
+    });
+
+    test("debe habilitar el boton de abandonar cuando la partida esta en curso", async () => {
+        render(<AbandonarPartida {...Started} />);
+        const boton = await screen.findByText("Abandonar");
+        expect(boton).not.toBeDisabled();
+    });
+
+    test("debe habilitar el botón de abandonar cuando la partida está en espera y el jugador no es anfitrión", async () => {
+        render(<AbandonarPartida {...WaitingNoAnfitrion} />);
+        const boton = await screen.findByText("Abandonar");
+        expect(boton).not.toBeDisabled();
+    });
+
+    test("debe deshabilitar el botón de abandonar cuando la partida está en espera y el jugador es anfitrión", async () => {
+        render(<AbandonarPartida {...WaitingAnfitrion} />);
+        const boton = await screen.findByText("Abandonar");
+        expect(boton).toBeDisabled();
+    });
+
+    test("debe fallar cuando el estado de la partida es diferente de WAITING o STARTED", async () => {
+        const consoleErrorSpy = jest.spyOn(console, 'error').mockImplementation(() => {});
+        render(<AbandonarPartida {...Error} />);
+        expect(consoleErrorSpy).toHaveBeenCalledWith("Estado de partida no permitido");
+        expect(screen.queryByText("Abandonar")).toBeNull();
+        consoleErrorSpy.mockRestore();
+    });
+
+    test("AbandonarPartida se ejecuta correctamente", async () => {
+        render(<AbandonarPartida {...Started} />);
+        const boton = await screen.findByText("Abandonar");
+        expect(boton).not.toBeDisabled();
+        fireEvent.click(boton);
+        await waitFor (() => {expect(mockedNavigate).toHaveBeenCalledTimes(1)});
+    });
+});

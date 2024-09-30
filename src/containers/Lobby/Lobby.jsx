bfd9b3389b01b03bf6afe0618bd9f0cb762ce943
Alfredo Viera <alfredo.viera@mi.unc.edu.ar>
2024-09-30 01:23:13 -0300

    SWT-31 Agrega manejo de empezar partida en websocket

--- src/containers/Lobby/Lobby.jsx
+++ src/containers/Lobby/Lobby.jsx
@@ -4,9 +4,12 @@ import { WEBSOCKET_URL } from "../../variablesConfiguracion";
 import Alerts from "../../components/Alerts";
 import { useParams } from "react-router-dom";
 import "./Lobby.css";
+import IniciarPartida from "./components/IniciarPartida";
+import { useNavigate } from "react-router-dom";
 
 function Lobby() {
   const { idPartida, idJugador } = useParams();
+  const navigate = useNavigate();
   const websocket_url = `${WEBSOCKET_URL}/${idPartida}/ws/${idJugador}`;
   const { lastJsonMessage } = useWebSocket(websocket_url, {
     share: true,
@@ -31,6 +34,9 @@ function Lobby() {
           setEstaShaking(true);
           setTimeout(() => setEstaShaking(false), 1000);
           break;
+        
+        case "START_MATCH":
+          navigate(`/matches/${idPartida}`);
 
         default:
           console.error("key incorrecto recibido del websocket");
@@ -44,6 +50,12 @@ function Lobby() {
       <div className={`${estaShaking ? "animate-shake" : ""}`}>
         {mostrarAlerta && <Alerts type={tipoAlerta} message={mensajeAlerta} />}
       </div>
+      <IniciarPartida 
+        idPartida={idPartida} 
+        idJugador={idJugador} 
+        nJugadoresEnLobby={2}
+        maxJugadores={3}
+      />
     </div>
   );
 }

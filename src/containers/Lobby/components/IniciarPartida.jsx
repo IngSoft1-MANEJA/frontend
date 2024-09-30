dd116fe4779505cf0deee1c3cf4085309271a87a
Alfredo Viera <alfredo.viera@mi.unc.edu.ar>
2024-09-30 01:22:14 -0300

    SWT-31 Agrega componente de IniciarPartida

---
+++ src/containers/Lobby/components/IniciarPartida.jsx
@@ -0,0 +1,42 @@
+import React from 'react';
+import { ServicioPartida } from '../../../services/ServicioPartida';
+import { useNavigate } from 'react-router-dom';
+
+const IniciarPartida = ({
+  idPartida,
+  idJugador,
+  nJugadoresEnLobby,
+  maxJugadores,
+}) => {
+  const habilitar = nJugadoresEnLobby === maxJugadores;
+  const navigate = useNavigate();
+
+  const manejarClick = async () => {
+    if (!habilitar) {
+      return;
+    }
+    try {
+      const respuesta = await ServicioPartida.iniciarPartida(
+        idPartida,
+        idJugador
+      );
+      console.log(respuesta);
+      navigate(`/matches/${idPartida}`);
+    } catch (error) {
+      console.error(error.message);
+    }
+  };
+  return (
+    <div>
+      <button
+        className="btn"
+        disabled={habilitar ? "" : "disabled"}
+        onClick={manejarClick}
+      >
+        Iniciar Partida
+      </button>
+    </div>
+  );
+};
+
+export default IniciarPartida;

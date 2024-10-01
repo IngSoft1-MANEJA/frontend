e6b416bd54b0ea80ab845eec1d7b6061340a964e
Nacho <ignacio.augusto.ramirez@mi.unc.edu.ar>
2024-10-01 18:00:20 -0300

    SWT-6 agrega Componente Ficha y renderizado Ficha de color

---
+++ src/containers/Game/components/Ficha.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import RedTile from '../../../assets/Colores/A.svg';
+import YellowTile from '../../../assets/Colores/B.svg';
+import GreenTile from '../../../assets/Colores/C.svg';
+import BlueTile from '../../../assets/Colores/D.svg';
+
+const images = {
+    red: RedTile,
+    yellow: YellowTile,
+    green: GreenTile,
+    blue: BlueTile,
+};
+
+export const Ficha = ({color}) => {
+    const tileImage = images[color];
+
+    return (
+        <div className="celda">
+            <img className="h-auto max-w-full" src={tileImage} alt={`${color}`} />
+        </div>
+    )
+}
+
+export default Tile;
--- src/containers/Game/components/Tablero.jsx
+++ src/containers/Game/components/Tablero.jsx
@@ -1,41 +1,23 @@
 import React from 'react'
-import RedTile from '../../../assets/Colores/A.svg';
-import YellowTile from '../../../assets/Colores/B.svg';
-import GreenTile from '../../../assets/Colores/C.svg';
-import BlueTile from '../../../assets/Colores/D.svg';
+import { Ficha } from './Ficha.jsx';
 import "./Tablero.css";
 
 export const Tablero = ({tiles}) => {
-    const rows = 6;
-    const columns = 6;
-    const size = rows * columns;
 
-    const images = {
-      red: RedTile,
-      yellow: YellowTile,
-      green: GreenTile,
-      blue: BlueTile,
-    };
-    
-    const crearGrilla = () => {
-      const gridItems = [];
-      for (let i = 0; i < size; i++) {
-        const tileColor = tiles[i % tiles.length];
-        const tileImage = images[tileColor];
-        gridItems.push(
-          <div key={i} className="cell">
-            <img className="h-auto max-w-full" src={tileImage} alt={`Tile ${i + 1}`} />
-          </div>
-        );
-      }
-      return gridItems;
-    };
-  
+    const gridCell = tiles.map((tile, index) => {
+      return (
+        <Ficha key={index} color={tile}/>
+      );
+    });
+
     return (
       <div className="tablero flex w-100 h-screen justify-center items-center">
         <div className="tablero-grid grid grid-cols-6 gap-1">
-          {crearGrilla()}
+          {gridCell}
         </div>
       </div>
     );
-  };
+};
+
+export default Tablero;
+

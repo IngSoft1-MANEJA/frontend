f8501d9c58e42c025779cb57cfbb85b833583ca1
Alexis714 <136205174+Alexis714@users.noreply.github.com>
2024-10-07 14:23:36 -0300

    SWT-3 agrega Cartas Movimiento Oponentes

--- src/containers/Game/components/cartasMovimiento.jsx
+++ src/containers/Game/components/cartasMovimiento.jsx
@@ -9,16 +9,17 @@ import mov4 from '../../../assets/movimientos/mov4.svg';
 import mov5 from '../../../assets/movimientos/mov5.svg';
 import mov6 from '../../../assets/movimientos/mov6.svg';
 import mov7 from '../../../assets/movimientos/mov7.svg';
+import backmov from '../../../assets/movimientos/back-mov.svg';
 import "./cartasMovimiento.css";
 
 const urlMap = {
-    "Diagonal": mov1,
-    "Inverse Diagonal": mov2,
-    "Line": mov3,
-    "Line Between": mov4,
-    "Line Border": mov5,
+    "DIAGONAL": mov1,
+    "INVERSE_DIAGONAL": mov2,
+    "LINE": mov3,
+    "LINE_BETWEEN": mov4,
+    "LINE_BORDER": mov5,
     "L": mov6,
-    "Inverse L": mov7
+    "INVERSE_L": mov7
 }
 
 export const CartasMovimiento = () => {
@@ -26,12 +27,14 @@ export const CartasMovimiento = () => {
     /*const websocket_url = `${WEBSOCKET_URL}/${match_id}/ws/${player_id}`;
     const { lastJsonMessage } = useWebSocket(websocket_url, { share: true });
     const [cartasMovimiento, setCartasMovimiento] = useState([]);
-
+    const [cartasOponente, setCartasOponente] = useState([]);
     
     useEffect(() => {
         if (lastJsonMessage !== null) {
             if (lastJsonMessage.key == "GET_MOVEMENT_CARD") {
                 setCartasMovimiento(lastJsonMessage.payload);
+            } else if (lastJsonMessage.key == "PLAYER_RECEIVE_MOVEMENT_CARD") {
+                setCartasOponente(lastJsonMessage.payload);
             } else {
                 console.error("key incorrecto recibido del websocket");
             }
@@ -39,22 +42,48 @@ export const CartasMovimiento = () => {
         }, [
             lastJsonMessage,
             setCartasMovimiento,
-        ]);*/
+        ]);
+        */
 
-    const cartasMovimiento = [
-        { name: "Diagonal" },
-        { name: "Inverse Diagonal" },
-        { name: "L" }];
-        
-    return (
-        <div className="cartas-movimientos">
-        {cartasMovimiento.map((carta, index) => (
-            <div key={index} className="carta-movimiento">
-                <img src={urlMap[carta.name]} alt={carta.name} />
+        const cartasMovimiento = [ {name: "DIAGONAL"}, {name: "INVERSE_DIAGONAL"}, {name: "LINE"}];
+        const Oponentes = [1, 2];
+
+        return (
+            <div className="cartas-movimientos">
+                <div className="cartas-movimientos-propias">
+                    {cartasMovimiento.map((carta, index) => (
+                        <div key={index} className="carta-movimiento">
+                            <img src={urlMap[carta.name]} alt={carta.name}/>
+                        </div>
+                    ))}
+                </div>
+                <div className="cartas-movimientos-oponente-arriba">
+                    {Array.from({ length: 3 }).map((_, index) => (
+                        <div key={index} className="carta-movimiento">
+                            <img src={backmov}/>
+                        </div>
+                    ))}
+                </div>
+                {Oponentes.length > 1 && (
+                <div className="cartas-movimientos-oponente-derecha">
+                    {Array.from({ length: 3 }).map((_, index) => (
+                        <div key={index} className="carta-movimiento">
+                            <img src={backmov}/>
+                        </div>
+                    ))}
+                </div>
+                )}
+                {Oponentes.length > 2 && (
+                <div className="cartas-movimientos-oponente-izquierda">
+                    {Array.from({ length: 3 }).map((_, index) => (
+                        <div key={index} className="carta-movimiento">
+                            <img src={backmov}/>
+                        </div>
+                    ))}
+                </div>
+                )}
             </div>
-        ))}
-        </div>
-    );
+        );
 }
 
 export default CartasMovimiento;

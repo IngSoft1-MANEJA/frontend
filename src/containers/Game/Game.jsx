84cb04171a7e9cc500a1ec28b6711c38744925a7
Nacho <ignacio.augusto.ramirez@mi.unc.edu.ar>
2024-10-08 18:03:48 -0300

    SWT-2 corrige Scroll en game por boton abandonar partida

--- src/containers/Game/Game.jsx
+++ src/containers/Game/Game.jsx
@@ -9,35 +9,26 @@ import { DatosJugadorContext } from "../../contexts/DatosJugadorContext";
 
 export function Game() {
   const { match_id } = useParams();
-  // const { datosJugador, setDatosJugador } = useContext(DatosJugadorContext);
-  // const [tiles, setTiles] = useState([]);
-  // const websocket_url = `${WEBSOCKET_URL}/${match_id}/ws/${datosJugador.player_id}`;
-  // const { lastJsonMessage } = useWebSocket(websocket_url, { share: true });
+  const { datosJugador, setDatosJugador } = useContext(DatosJugadorContext);
+  const [tiles, setTiles] = useState([]);
+  const websocket_url = `${WEBSOCKET_URL}/${match_id}/ws/${datosJugador.player_id}`;
+  const { lastJsonMessage } = useWebSocket(websocket_url, { share: true });
 
-  // useEffect(() => {
-  //   if (lastJsonMessage !== null) {
-  //       if (lastJsonMessage.key == "START_MATCH") {
-  //           setTiles(lastJsonMessage.payload.board);
-  //       } else {
-  //           console.error("key incorrecto recibido del websocket");
-  //       }
-  //       }
-  //   }, [
-  //       lastJsonMessage,
-  //       setTiles,
-  // ]);
-
-  const tiles = [
-    ['grey', 'red', 'green', 'yellow', 'red', 'yellow'], 
-    ['green', 'blue', 'red', 'yellow', 'green', 'blue'], 
-    ['red', 'yellow', 'blue', 'blue', 'blue', 'yellow'], 
-    ['green', 'blue', 'red', 'yellow', 'green', 'blue'], 
-    ['red', 'yellow', 'green', 'yellow', 'green', 'green'], 
-    ['green', 'blue', 'blue', 'yellow', 'green', 'blue']
-  ];
+  useEffect(() => {
+    if (lastJsonMessage !== null) {
+        if (lastJsonMessage.key == "START_MATCH") {
+            setTiles(lastJsonMessage.payload.board);
+        } else {
+            console.error("key incorrecto recibido del websocket");
+        }
+        }
+    }, [
+        lastJsonMessage,
+        setTiles,
+  ]);
 
   return (
-    <div className="game-div w-full h-screen m-0">
+    <div className="game-div relative w-full h-screen m-0">
       <Tablero tiles={tiles}/>
       <AbandonarPartida
         estadoPartida="STARTED"

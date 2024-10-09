53234733e1058970eeb2377d53bcec3cb138271d
Alfredo Viera <alfredo.viera@mi.unc.edu.ar>
2024-10-09 11:06:36 -0300

    SWT-57 Maneja evento de ganar por forfeit

--- src/containers/Game/Game.jsx
+++ src/containers/Game/Game.jsx
@@ -1,13 +1,33 @@
-import React from "react";
+import React, { useEffect } from "react";
 
-/*import { useContext } from "react";
+import { useContext } from "react";
 import { useParams } from "react-router-dom";
-import { AbandonarPartida } from "../../components/AbandonarPartida";
-import { DatosJugadorContext } from "../../contexts/DatosJugadorContext";*/
+// import { AbandonarPartida } from "../../components/AbandonarPartida";
+import { DatosJugadorContext } from "../../contexts/DatosJugadorContext";
+import { crearWebsocket, WebsocketEvents } from "../../services/ServicioWebsocket";
+import { JugadorGanoMotivo } from "../../services/ServicioPartida";
 
 export function Game() {
-  /*const { datosJugador, setDatosJugador } = useContext(DatosJugadorContext);
-  const { match_id } = useParams();*/
+  const { datosJugador, setDatosJugador } = useContext(DatosJugadorContext);
+  const { match_id } = useParams();
+  const { LastJsonMessage } = crearWebsocket(match_id, datosJugador.player_id);
+
+  useEffect(() => {
+    if (LastJsonMessage !== null) {
+      switch (LastJsonMessage.key) {
+        case WebsocketEvents.WINNER:
+          if (LastJsonMessage.payload.reason === JugadorGanoMotivo.FORFEIT) {
+            setDatosJugador({ player_id: null, is_owner: false });
+            setDatosPartida({ max_players: 2 });
+            Navigate("/");
+          }
+          break;
+      
+        default:
+          break;
+      }
+    }
+  }, [LastJsonMessage]);
 
   return (
     <div>

34f8f83c90ede7e8689d8d0783e034014ec9fa24
Alexis714 <136205174+Alexis714@users.noreply.github.com>
2024-10-11 14:26:22 -0300

    SWT-63 corrige URL de Websocket

--- src/containers/Game/components/CartasFiguras.jsx
+++ src/containers/Game/components/CartasFiguras.jsx
@@ -67,7 +67,7 @@ export const CartasFiguras = () => {
   const { match_id } = useParams();
   const { datosJugador, setDatosJugador } = useContext(DatosJugadorContext);
 
-  const websocket_url = `${WEBSOCKET_URL}/${match_id}/ws/${datosJugador.player_id}`;
+  const websocket_url = `${WEBSOCKET_URL}/matches/${match_id}/ws/${datosJugador.player_id}`;
   const { lastJsonMessage } = useWebSocket(websocket_url, { share: true });
   const [cartasFiguras, setCartasFiguras] = useState([]);
   const [miTurno, setMiTurno] = useState(0);

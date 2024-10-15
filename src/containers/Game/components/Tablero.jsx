c9f57c11648b96f938a3ab9ce92a6a52a6aa58c8
Alexis714 <136205174+Alexis714@users.noreply.github.com>
2024-10-15 02:08:58 -0300

    SWT-35 agrega SWAP de colores

--- src/containers/Game/components/Tablero.jsx
+++ src/containers/Game/components/Tablero.jsx
@@ -1,16 +1,129 @@
 import React from 'react'
+import { useEffect, useContext, useState } from 'react';
+import { useParams } from 'react-router-dom';
+import useWebSocket from "react-use-websocket";
+import { WEBSOCKET_URL } from "../../../variablesConfiguracion.js";
 import { Ficha } from './Ficha.jsx';
+import { Alerts } from "../../../components/Alerts";
 import "./Tablero.css";
+import { UsarMovimientoContext } from '../../../contexts/UsarMovimientoContext.jsx';
+import { ServicioPartida } from "../../../services/ServicioPartida.js";
 
-export const Tablero = ({tiles}) => {
+export const Tablero = ({ initialTiles }) => {
+  const { match_id } = useParams();
+  const { usarMovimiento, setUsarMovimiento } = useContext(UsarMovimientoContext);
+
+  const [tiles, setTiles] = useState(initialTiles);
+  const [mostrarAlerta, setMostrarAlerta] = useState(false);
+  const [mensajeAlerta, setMensajeAlerta] = useState("");
+
+  const swapColors = (fichasSeleccionadas) => {
+    console.log("swapColors");
+
+
+    console.log("Fichas seleccionadas:", fichasSeleccionadas); // Añadir esto
+
+    if (fichasSeleccionadas.length === 2) {
+      const [ficha1, ficha2] = fichasSeleccionadas;
+
+      const { rowIndex: filaFicha1, columnIndex: columnaFicha1 } = ficha1;
+      const { rowIndex: filaFicha2, columnIndex: columnaFicha2 } = ficha2;
+
+      const newTiles = tiles.map(row => [...row]);
+
+      const temp = newTiles[filaFicha1][columnaFicha1];
+      newTiles[filaFicha1][columnaFicha1] = newTiles[filaFicha2][columnaFicha2];
+      newTiles[filaFicha2][columnaFicha2] = temp;
+
+      setTiles(newTiles);
+
+      setUsarMovimiento({
+        ...usarMovimiento,
+        fichasSeleccionadas: [],
+        cartaSeleccionada: null,
+      });
+    }
+  };
+
+  const handleFichaClick = async (rowIndex, columnIndex) => {
+
+    if (usarMovimiento.cartaSeleccionada !== null) {
+      const fichaEstaSeleccionada = usarMovimiento.fichasSeleccionadas.some(ficha => ficha.rowIndex === rowIndex && ficha.columnIndex === columnIndex);
+
+      if (fichaEstaSeleccionada) {
+        // si la ficha ya esta seleccionada, deseleccionarla al hacer click nuevamente
+        const newFichasSeleccionadas = usarMovimiento.fichasSeleccionadas.filter(ficha => ficha.rowIndex !== rowIndex || ficha.columnIndex !== columnIndex);
+        setUsarMovimiento({ ...usarMovimiento, fichasSeleccionadas: newFichasSeleccionadas });
+      } 
+      else if (usarMovimiento.fichasSeleccionadas.length < 2) {
+        // si la ficha no esta seleccionada, seleccionarla
+        const newFichasSeleccionadas = [...usarMovimiento.fichasSeleccionadas, { rowIndex, columnIndex }];
+        setUsarMovimiento({ ...usarMovimiento, fichasSeleccionadas: newFichasSeleccionadas });
+
+        if (newFichasSeleccionadas.length === 2) {
+          try {
+            const resJson = await ServicioPartida.validarMovimiento(
+              match_id,
+              newFichasSeleccionadas,
+              usarMovimiento.cartaSeleccionada
+            );
+            console.log(resJson);
+            if (resJson.isValid) {
+              // Realizar animacion de swap.
+              swapColors(newFichasSeleccionadas);
+              // Actualizar selecciones
+              setTimeout(() => {
+                setUsarMovimiento({
+                  ...usarMovimiento,
+                  fichasSeleccionadas: [],
+                  cartaSeleccionada: null,
+                  cartasUsadas: [...usarMovimiento.cartasUsadas, usarMovimiento.cartaSeleccionada],
+                  highlightCarta: { state: false, key: '' },
+                });
+              }, 700);
+            } else {
+              setMensajeAlerta("Movimiento inválido");
+              setMostrarAlerta(true);
+              setUsarMovimiento({
+                ...usarMovimiento,
+                fichasSeleccionadas: [],
+              });
+              setTimeout(() => {
+                setMostrarAlerta(false);
+              },1000);
+              console.log('Movimiento inválido');
+            }
+          } catch (err) {
+            setMensajeAlerta("Error creando sala de partida");
+            console.log(err);
+          }
+        }
+      }
+    }
+    else {
+      setUsarMovimiento({ ...usarMovimiento, fichasSeleccionadas: [] });
+    }
+  };
+
+  const estaHighlighted = (rowIndex, columnIndex) => {
+    return usarMovimiento.fichasSeleccionadas.some(ficha => ficha.rowIndex === rowIndex && ficha.columnIndex === columnIndex);
+  };
 
   const gridCell = tiles.map((row, rowIndex) => {
     return row.map((tileColor, columnIndex) => {
-      return <Ficha key={`${rowIndex}-${columnIndex}`} color={tileColor}/>
+      return (
+        <Ficha 
+          key={`${rowIndex}-${columnIndex}`}
+          color={tileColor} 
+          onClick={() => handleFichaClick(rowIndex, columnIndex)}
+          highlightClass={estaHighlighted(rowIndex, columnIndex)}
+        />
+      );
     });
   });
   return (
     <div className="tablero flex w-100 h-screen justify-center items-center">
+      {mostrarAlerta && <Alerts type={'error'} message={mensajeAlerta} />}
       <div className="tablero-grid grid grid-cols-6 gap-1">
         {gridCell}
       </div>

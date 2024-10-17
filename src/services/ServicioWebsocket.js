9b1085a14c6db44500c80ff98d63d3f5354fc720
Alfredo Viera <alfredo.viera@mi.unc.edu.ar>
2024-10-16 23:35:04 -0300

    SWT-57 Elimina funcion no usada

--- src/services/ServicioWebsocket.js
+++ src/services/ServicioWebsocket.js
@@ -2,18 +2,8 @@ import "react-use-websocket";
 import { WEBSOCKET_URL } from "../variablesConfiguracion";
 
 export const WebsocketEvents = Object.freeze({
-    WINNER: "WINNER",
-    PLAYER_JOIN: "PLAYER_JOIN",
-    PLAYER_LEFT: "PLAYER_LEFT",
-    START_MATCH: "START_MATCH",
+  WINNER: "WINNER",
+  PLAYER_JOIN: "PLAYER_JOIN",
+  PLAYER_LEFT: "PLAYER_LEFT",
+  START_MATCH: "START_MATCH",
 });
-
-export const crearWebsocket = (match_id, player_id) => {
-  const websocket_url = `${WEBSOCKET_URL}/matches/${match_id}/ws/${player_id}`;
-  return useWebSocket(websocket_url, {
-    share: true,
-    onClose: () => console.log("Websocket - Lobby: conexión cerrada."),
-    onError: (event) => console.error("Websocket - Lobby: error: ", event),
-    onOpen: () => console.log("Websocket - Lobby: conexión abierta."),
-  });
-}

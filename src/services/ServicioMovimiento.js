517875e8eece4988b558c1016bbc31e4a8cd0cd0
Alfredo Viera <alfredo.viera@mi.unc.edu.ar>
2024-10-18 00:47:48 -0300

    SWT-36 Agrega funcion para deshacer movimiento

--- src/services/ServicioMovimiento.js
+++ src/services/ServicioMovimiento.js
@@ -33,6 +33,39 @@ const llamarServicio = async (match_id, player_id, newFichasSeleccionadas, carta
   }
 };
 
+/**
+ * Deshace el último movimiento parcial realizado por el jugador. Muestra una alerta en caso de error. 
+ * 
+ * @param {string} match_id - ID de la partida.
+ * @param {string} player_id - ID del jugador.
+ * @param {function} setUsarMovimiento - Función para actualizar el estado de usarMovimiento.
+ * @param {function} setMensajeAlerta - Función para actualizar el mensaje de alerta.
+ * @param {function} setMostrarAlerta - Función para mostrar la alerta en caso de error.
+ */
+const deshacerMovimiento = async (match_id, player_id, setUsarMovimiento, setMensajeAlerta, setMostrarAlerta) => {
+  try {
+    const resJson = await ServicioPartida.deshacerMovimientoParcial(match_id, player_id);
+    console.log(resJson);
+
+    const cartaADeshacer = resJson.payload.movement_card;
+
+    setUsarMovimiento(prev => ({
+      ...prev,
+      cartasUsadas: prev.cartasUsadas.filter(carta => carta !== cartaADeshacer[1]),
+    }));
+
+    // TODO: Agregar lógica para deshacer movimiento parcial en el tablero.
+
+  } catch (err) {
+    setMensajeAlerta("Error al deshacer movimiento");
+    setMostrarAlerta(true);
+    setTimeout(() => {
+      setMostrarAlerta(false);
+    }, 1000);
+    console.log(err);
+  }
+};
+
 const estaHighlighted = (rowIndex, columnIndex, fichasSeleccionadas) => {
   return fichasSeleccionadas.some(ficha => ficha.rowIndex === rowIndex && ficha.columnIndex === columnIndex);
 };
@@ -154,6 +187,7 @@ function calcularMovimientos (rowIndex, columnIndex, carta) {
 export const ServicioMovimiento = {
   calcularMovimientos,
   llamarServicio,
+  deshacerMovimiento,
   estaHighlighted,
   esMovimientoPosible
 };

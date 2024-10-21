d2a48550ba5f3e5c108918c68d8b55e6b2e16e90
Alfredo Viera <alfredo.viera@mi.unc.edu.ar>
2024-10-21 02:19:35 -0300

    SWT-40 deshabilita fichas para seleecionar en cartaFiguraSeleccionada

--- src/containers/Game/components/Ficha.jsx
+++ src/containers/Game/components/Ficha.jsx
@@ -5,6 +5,7 @@ import YellowTile from "../../../assets/Colores/B.svg";
 import GreenTile from "../../../assets/Colores/C.svg";
 import BlueTile from "../../../assets/Colores/D.svg";
 import { UsarMovimientoContext } from "../../../contexts/UsarMovimientoContext.jsx";
+import { CompletarFiguraContext } from "../../../contexts/CompletarFiguraContext.jsx";
 
 const images = {
   red: RedTile,
@@ -26,6 +27,7 @@ export const Ficha = ({
   const { usarMovimiento, setUsarMovimiento } = useContext(
     UsarMovimientoContext,
   );
+  const { cartaSeleccionada: cartaFiguraSeleccionada } = useContext(CompletarFiguraContext);
 
   const fichaEstaSeleccionada = usarMovimiento.fichasSeleccionadas.length > 0;
 
@@ -40,7 +42,7 @@ export const Ficha = ({
       }
       className={`celda
                 ${usarMovimiento.fichaHovering && !highlightClass ? "hover:cursor-pointer hover:shadow-[0px_0px_12px_rgba(224,138,44,1)] hover:scale-105" : ""} 
-                ${disabled && fichaEstaSeleccionada ? "opacity-40 pointer-events-none" : ""}
+                ${(disabled && fichaEstaSeleccionada) || (!highlightFiguraInicial && cartaFiguraSeleccionada !== null) ? "opacity-40 pointer-events-none" : ""}
                 ${highlightClass ? "cursor-pointer shadow-[0px_0px_12px_rgba(17,195,22,.8),0px_0px_20px_rgba(31,222,37,1)] scale-105" : ""}
                 ${usarMovimiento.fichaHovering && movimientoPosible ? "hover:cursor-pointer hover:shadow-[0px_0px_12px_rgba(224,138,44,1),0px_0px_12px_rgba(224,138,44,1)] hover:scale-105" : ""}
                 ${movimientoPosible && fichaEstaSeleccionada ? "animate-breathing" : ""}  

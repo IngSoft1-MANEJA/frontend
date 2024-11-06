3c47013bfeaa0113f1ea8d188cdc08ab1eaeddaa
Ignacio <ignacio.augusto.ramirez@mi.unc.edu.ar>
2024-11-05 21:15:42 -0300

    SWT-46 corrige estilos

--- src/containers/Game/components/Registro.jsx
+++ src/containers/Game/components/Registro.jsx
@@ -175,7 +175,7 @@ export const Registro = () => {
     });
 
   return (
-    <div className="registro-container absolute h-4/6 -translate-y-1/2 left-5 top-1/2 z-50 w-60 p-1 justify-center">
+    <div className="registro-container absolute h-4/6 -translate-y-1/2 left-5 top-1/2 z-50 w-1/5 p-1 justify-center">
       <div className="registro-container-inner relative bg-base-200 flex flex-col h-full w-full items-center">
         <div className="chatbox overflow-auto w-full flex flex-col-reverse">
           {registroMessage}

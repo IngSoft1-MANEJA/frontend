7564f2bf77eeb5e87d37be65a44be9aa4ea83496
Alfredo Viera <alfredo.viera@mi.unc.edu.ar>
2024-11-07 20:55:33 -0300

    SWT-91 Formatea codigo

--- src/__test__/ListaPartidas.test.js
+++ src/__test__/ListaPartidas.test.js
@@ -1,20 +1,11 @@
 import React from "react";
 import { jest } from "@jest/globals";
-import {
-  render,
-  screen,
-  waitFor,
-  cleanup,
-} from "@testing-library/react";
+import { render, screen, waitFor, cleanup } from "@testing-library/react";
 import "@testing-library/jest-dom/extend-expect";
 import { ListaPartidas } from "../containers/App/components/ListaPartidas.jsx";
 import { ListarPartidasMock } from "../__mocks__/ListarPartidas.mock.js";
-import {
-  DatosJugadorProvider,
-} from "../contexts/DatosJugadorContext.jsx";
-import {
-  DatosPartidaProvider,
-} from "../contexts/DatosPartidaContext.jsx";
+import { DatosJugadorProvider } from "../contexts/DatosJugadorContext.jsx";
+import { DatosPartidaProvider } from "../contexts/DatosPartidaContext.jsx";
 import useWebSocket from "react-use-websocket";
 
 const mockedUsedNavigate = jest.fn();
@@ -31,7 +22,7 @@ jest.mock("react-use-websocket", () => ({
 
 const mockMatchList = {
   key: "MATCHES_LIST",
-  payload: { matches: ListarPartidasMock }
+  payload: { matches: ListarPartidasMock },
 };
 
 describe("ListarPartidas", () => {
@@ -46,12 +37,11 @@ describe("ListarPartidas", () => {
         <DatosJugadorProvider>
           <ListaPartidas />
         </DatosJugadorProvider>
-      </DatosPartidaProvider>
+      </DatosPartidaProvider>,
     );
-  }
+  };
 
   test("debe renderizar las partidas correctamente", () => {
-
     useWebSocket.mockImplementation((url) => ({
       lastJsonMessage: mockMatchList,
     }));
@@ -59,9 +49,7 @@ describe("ListarPartidas", () => {
     customRender();
 
     ListarPartidasMock.forEach((partida) => {
-      expect(
-        screen.getByText(partida.id.toString()),
-      ).toBeInTheDocument();
+      expect(screen.getByText(partida.id.toString())).toBeInTheDocument();
       expect(screen.getByText(partida.match_name)).toBeInTheDocument();
       expect(
         screen.getByText(`${partida.current_players}/${partida.max_players}`),
@@ -77,9 +65,7 @@ describe("ListarPartidas", () => {
     const { rerender } = customRender();
 
     ListarPartidasMock.forEach((partida) => {
-      expect(
-        screen.getByText(partida.id.toString()),
-      ).toBeInTheDocument();
+      expect(screen.getByText(partida.id.toString())).toBeInTheDocument();
       expect(screen.getByText(partida.match_name)).toBeInTheDocument();
       expect(
         screen.getByText(`${partida.current_players}/${partida.max_players}`),
@@ -102,9 +88,9 @@ describe("ListarPartidas", () => {
             current_players: 1,
             match_name: "Partida 4",
           },
-        ]
-      }
-    }
+        ],
+      },
+    };
 
     useWebSocket.mockImplementation((url) => ({
       lastJsonMessage: nuevaListaEvento,
@@ -115,23 +101,18 @@ describe("ListarPartidas", () => {
         <DatosJugadorProvider>
           <ListaPartidas />
         </DatosJugadorProvider>
-      </DatosPartidaProvider>
+      </DatosPartidaProvider>,
     );
-
 
     expect(screen.queryByText("Partida 1")).not.toBeInTheDocument();
     expect(screen.queryByText("Partida 2")).not.toBeInTheDocument();
 
     nuevaListaEvento.payload.matches.forEach((partida) => {
-      expect(
-        screen.getByText(partida.id.toString()),
-      ).toBeInTheDocument();
+      expect(screen.getByText(partida.id.toString())).toBeInTheDocument();
       expect(screen.getByText(partida.match_name)).toBeInTheDocument();
       expect(
         screen.getByText(`${partida.current_players}/${partida.max_players}`),
       ).toBeInTheDocument();
     });
-
-
   });
 });

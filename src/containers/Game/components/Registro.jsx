561e2014ae99c4dcfac9c21221d28c2bed9f147a
Ignacio <ignacio.augusto.ramirez@mi.unc.edu.ar>
2024-11-09 13:10:14 -0300

    SWT-74 agrega Servicio para registro

--- src/containers/Game/components/Registro.jsx
+++ src/containers/Game/components/Registro.jsx
@@ -2,9 +2,7 @@ import React, { useState, useContext, useEffect } from "react";
 import { DatosJugadorContext } from "../../../contexts/DatosJugadorContext.jsx";
 import { DatosPartidaContext } from "../../../contexts/DatosPartidaContext.jsx";
 import { EventoContext } from "../../../contexts/EventoContext.jsx";
-import { WebsocketEvents } from "../../../services/ServicioWebsocket";
-import { JugadorGanoMotivo } from "../../../services/ServicioPartida";
-import { ServicioFigura } from "../../../services/ServicioFigura";
+import { ServicioRegistro } from "../../../services/ServicioRegistro.js";
 import "./Registro.css";
 
 export const Registro = ({ sendJsonMessage, lastMessage }) => {
@@ -31,131 +29,7 @@ export const Registro = ({ sendJsonMessage, lastMessage }) => {
       if (eventQueue.length > 0) {
         const [currentEvent, ...remainingQueue] = eventQueue;
         setEventQueue(remainingQueue);
-
-        switch (currentEvent.key) {
-          case "GET_PLAYER_MATCH_INFO":
-            setRegistro((prevRegistro) => [
-              ...prevRegistro,
-              {
-                mensaje:
-                  "Te has unido a la partida, tu orden de turno es: " +
-                  currentEvent.payload.turn_order,
-                tipo: "evento",
-              },
-            ]);
-            break;
-          case "PLAYER_RECEIVE_SHAPE_CARD":
-            if (currentEvent.payload[0].shape_cards.length > 0) {
-              if (currentEvent.payload[0].shape_cards.length === 1) {
-                setRegistro((prevRegistro) => [
-                  ...prevRegistro,
-                  {
-                    mensaje: `El jugador "${currentEvent.payload[0].player}" ha recibido la carta de figura "${currentEvent.payload[0].shape_cards[0][1]}".`,
-                    tipo: "evento",
-                  },
-                ]);
-              } else {
-                setRegistro((prevRegistro) => [
-                  ...prevRegistro,
-                  {
-                    mensaje: `El jugador "${currentEvent.payload[0].player}" ha recibido las cartas de figura "${currentEvent.payload[0].shape_cards.map((figura) => ServicioFigura.cartaStringName(figura[1])).join(", ")}".`,
-                    tipo: "evento",
-                  },
-                ]);
-              }
-            }
-            break;
-          case "END_PLAYER_TURN":
-            setRegistro((prevRegistro) => [
-              ...prevRegistro,
-              {
-                mensaje: `El jugador "${currentEvent.payload.current_player_name}" ha terminado su turno.`,
-                tipo: "evento",
-              },
-              {
-                mensaje: `Turno de "${currentEvent.payload.next_player_name}".`,
-                tipo: "evento",
-              },
-            ]);
-            break;
-          case "PLAYER_RECEIVE_NEW_BOARD":
-            setRegistro((prevRegistro) => [
-              ...prevRegistro,
-              {
-                mensaje: `El jugador "${datosPartida.current_player_name}" ha realizado un movimiento.`,
-                tipo: "evento",
-              },
-            ]);
-            break;
-          case "COMPLETED_FIGURE":
-            setRegistro((prevRegistro) => [
-              ...prevRegistro,
-              {
-                mensaje: `El jugador "${datosPartida.current_player_name}" ha completado la figura "${ServicioFigura.cartaStringName(currentEvent.payload.figure_name)}".`,
-                tipo: "evento",
-              },
-            ]);
-            break;
-          case "PLAYER_LEFT":
-            setRegistro((prevRegistro) => [
-              ...prevRegistro,
-              {
-                mensaje: `El jugador "${currentEvent.payload.name}" ha abandonado la partida.`,
-                tipo: "evento",
-              },
-            ]);
-            break;
-          case "PLAYER_SEND_MESSAGE":
-            setRegistro((prevRegistro) => [
-              ...prevRegistro,
-              {
-                mensaje: `${currentEvent.payload.message}`,
-                tipo: "chat",
-              },
-            ]);
-            break;
-          case WebsocketEvents.UNDO_PARTIAL_MOVE:
-            setRegistro((prevRegistro) => [
-              ...prevRegistro,
-              {
-                mensaje: `El jugador "${datosPartida.current_player_name}" ha deshecho un movimiento.`,
-                tipo: "evento",
-              },
-            ]);
-            break;
-          case WebsocketEvents.WINNER:
-            if (currentEvent.payload.reason === JugadorGanoMotivo.FORFEIT) {
-              setRegistro((prevRegistro) => [
-                ...prevRegistro,
-                {
-                  mensaje: "Has ganado la partida!",
-                  tipo: "evento",
-                },
-              ]);
-            }
-            if (currentEvent.payload.reason === JugadorGanoMotivo.NORMAL) {
-              if (datosJugador.player_id === currentEvent.payload.player_id) {
-                setRegistro((prevRegistro) => [
-                  ...prevRegistro,
-                  {
-                    mensaje: "Has ganado la partida!",
-                    tipo: "evento",
-                  },
-                ]);
-              } else {
-                setRegistro((prevRegistro) => [
-                  ...prevRegistro,
-                  {
-                    mensaje: "Has perdido la partida.",
-                    tipo: "evento",
-                  },
-                ]);
-              }
-            }
-            break;
-          default:
-            break;
-        }
+        ServicioRegistro.procesarMensajeEvento(currentEvent, setRegistro, datosJugador, datosPartida);
       }
     }, 150);
 
@@ -202,10 +76,10 @@ export const Registro = ({ sendJsonMessage, lastMessage }) => {
         return (
           <div key={index} className="registro-message">
             <div className={`chat ${isPlayerMessage ? "chat-start" : "chat-end"} text-sm`}>
-              <div className="chat-header pb-1">
+              <div className="chat-header pb-1 mb-1">
                 {isPlayerMessage ? "Tu" :  player_name}
               </div>
-              <div className="chat-bubble">
+              <div className="chat-bubble mb-2">
                 <p>{message.mensaje}</p>
               </div>
             </div>
@@ -215,7 +89,7 @@ export const Registro = ({ sendJsonMessage, lastMessage }) => {
       if (message.tipo === "evento") {
         return (
           <div key={index} className="registro-message">
-            <p className="chat-divider text-sm mb-2 pt-1 pb-1 pl-4 text-left bg-base-300">
+            <p className="chat-divider text-sm mb-1 pt-1 pb-1 pl-4 text-left bg-base-300">
               {message.mensaje}
             </p>
           </div>
@@ -232,6 +106,7 @@ export const Registro = ({ sendJsonMessage, lastMessage }) => {
         <textarea 
           className="textarea absolute bottom-0 w-full resize-none mt-2" 
           placeholder="Comenta"
+          value={messageText}
           onChange={(e) => setMessageText(e.target.value)}
           onKeyDown={handleKeyDown}>
         </textarea>
---
+++ src/services/ServicioRegistro.js
@@ -0,0 +1,134 @@
+import { WebsocketEvents } from "./ServicioWebsocket";
+import { JugadorGanoMotivo } from "./ServicioPartida";
+import { ServicioFigura } from "./ServicioFigura";
+
+function procesarMensajeEvento (currentEvent, setRegistro, datosJugador, datosPartida){
+    switch (currentEvent.key) {
+        case "GET_PLAYER_MATCH_INFO":
+            setRegistro((prevRegistro) => [
+                ...prevRegistro,
+                {
+                mensaje:
+                    "Te has unido a la partida, tu orden de turno es: " +
+                    currentEvent.payload.turn_order,
+                tipo: "evento",
+                },
+            ]);
+            break;
+        case "PLAYER_RECEIVE_SHAPE_CARD":
+            if (currentEvent.payload[0].shape_cards.length > 0) {
+                if (currentEvent.payload[0].shape_cards.length === 1) {
+                setRegistro((prevRegistro) => [
+                    ...prevRegistro,
+                    {
+                    mensaje: `El jugador "${currentEvent.payload[0].player}" ha recibido la carta de figura "${currentEvent.payload[0].shape_cards[0][1]}".`,
+                    tipo: "evento",
+                    },
+                ]);
+                } else {
+                setRegistro((prevRegistro) => [
+                    ...prevRegistro,
+                    {
+                    mensaje: `El jugador "${currentEvent.payload[0].player}" ha recibido las cartas de figura "${currentEvent.payload[0].shape_cards.map((figura) => ServicioFigura.cartaStringName(figura[1])).join(", ")}".`,
+                    tipo: "evento",
+                    },
+                ]);
+                }
+            }
+            break;
+        case "END_PLAYER_TURN":
+            setRegistro((prevRegistro) => [
+                ...prevRegistro,
+                {
+                mensaje: `El jugador "${currentEvent.payload.current_player_name}" ha terminado su turno.`,
+                tipo: "evento",
+                },
+                {
+                mensaje: `Turno de "${currentEvent.payload.next_player_name}".`,
+                tipo: "evento",
+                },
+            ]);
+            break;
+        case "PLAYER_RECEIVE_NEW_BOARD":
+            setRegistro((prevRegistro) => [
+                ...prevRegistro,
+                {
+                mensaje: `El jugador "${datosPartida.current_player_name}" ha realizado un movimiento.`,
+                tipo: "evento",
+                },
+            ]);
+            break;
+        case "COMPLETED_FIGURE":
+            setRegistro((prevRegistro) => [
+                ...prevRegistro,
+                {
+                mensaje: `El jugador "${datosPartida.current_player_name}" ha completado la figura "${ServicioFigura.cartaStringName(currentEvent.payload.figure_name)}".`,
+                tipo: "evento",
+                },
+            ]);
+            break;
+        case "PLAYER_LEFT":
+            setRegistro((prevRegistro) => [
+                ...prevRegistro,
+                {
+                mensaje: `El jugador "${currentEvent.payload.name}" ha abandonado la partida.`,
+                tipo: "evento",
+                },
+            ]);
+            break;
+        case "PLAYER_SEND_MESSAGE":
+            setRegistro((prevRegistro) => [
+                ...prevRegistro,
+                {
+                mensaje: `${currentEvent.payload.message}`,
+                tipo: "chat",
+                },
+            ]);
+            break;
+        case WebsocketEvents.UNDO_PARTIAL_MOVE:
+            setRegistro((prevRegistro) => [
+                ...prevRegistro,
+                {
+                mensaje: `El jugador "${datosPartida.current_player_name}" ha deshecho un movimiento.`,
+                tipo: "evento",
+                },
+            ]);
+            break;
+        case WebsocketEvents.WINNER:
+            if (currentEvent.payload.reason === JugadorGanoMotivo.FORFEIT) {
+                setRegistro((prevRegistro) => [
+                ...prevRegistro,
+                {
+                    mensaje: "Has ganado la partida!",
+                    tipo: "evento",
+                },
+                ]);
+            }
+            if (currentEvent.payload.reason === JugadorGanoMotivo.NORMAL) {
+                if (datosJugador.player_id === currentEvent.payload.player_id) {
+                setRegistro((prevRegistro) => [
+                    ...prevRegistro,
+                    {
+                    mensaje: "Has ganado la partida!",
+                    tipo: "evento",
+                    },
+                ]);
+                } else {
+                setRegistro((prevRegistro) => [
+                    ...prevRegistro,
+                    {
+                    mensaje: "Has perdido la partida.",
+                    tipo: "evento",
+                    },
+                ]);
+                }
+            }
+            break;
+        default:
+            break;
+    }
+}
+
+export const ServicioRegistro = {
+    procesarMensajeEvento
+  };

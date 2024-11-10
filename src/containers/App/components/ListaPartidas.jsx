b1eaa8e944eddb9bec362342c614c9263217fbeb
Alexis714 <136205174+Alexis714@users.noreply.github.com>
2024-11-10 12:45:57 -0300

    Corrige key en mensaje de ws enviado por front

--- src/containers/App/components/ListaPartidas.jsx
+++ src/containers/App/components/ListaPartidas.jsx
@@ -28,7 +28,7 @@ export const ListaPartidas = () => {
 
   const filtrarPorNombrePartida = (nombrePartida) => {
     // TODO: revisar el key y payload con el back.
-    sendJsonMessage({ key: "FILTER_MATCH", payload: { "match_name": nombrePartida } });
+    sendJsonMessage({ key: "FILTER_MATCHES", payload: { "match_name": nombrePartida } });
   };
 
 

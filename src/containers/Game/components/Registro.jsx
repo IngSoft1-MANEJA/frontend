dff47a9aab974979c2b751403c89d3d3323476fc
Alfredo Viera <alfredo.viera@mi.unc.edu.ar>
2024-11-10 02:00:57 -0300

    SWT-76 Ajusta posicion del chat

--- src/containers/Game/components/Registro.jsx
+++ src/containers/Game/components/Registro.jsx
@@ -179,7 +179,7 @@ export const Registro = () => {
     });
 
   return (
-    <div className="registro-container absolute h-3/5 -translate-y-1/2 left-5 top-1/2 z-50 w-1/5 p-1 justify-center">
+    <div className="registro-container absolute h-3/5 -translate-y-2/3 left-5 top-2/3 z-50 w-1/5 p-1 justify-center">
       <div className="registro-container-inner relative bg-base-200 flex flex-col h-full w-full items-center">
         <div className="chatbox overflow-auto w-full flex flex-col-reverse">
           {registroMessage}

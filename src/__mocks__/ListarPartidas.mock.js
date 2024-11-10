94a19ba07429f14d74a500ec031e5fd1cb56f433
Alexis714 <136205174+Alexis714@users.noreply.github.com>
2024-11-10 20:48:25 -0300

    SWT-68 recupera mock de listar partidas

---
+++ src/__mocks__/ListarPartidas.mock.js
@@ -0,0 +1,30 @@
+export const ListarPartidasMock = [
+    {
+      id: 1,
+      match_id: 1,
+      max_players: 3,
+      current_players: 2,
+      match_name: "Partida 1",
+    },
+    {
+      id: 2,
+      match_id: 2,
+      max_players: 4,
+      current_players: 4,
+      match_name: "Partida 2",
+    },
+    {
+      id: 3,
+      match_id: 3,
+      max_players: 4,
+      current_players: 2,
+      match_name: "Partida 3",
+    },
+    {
+      id: 4,
+      match_id: 4,
+      max_players: 2,
+      current_players: 1,
+      match_name: "Partida 4",
+    },
+  ];
